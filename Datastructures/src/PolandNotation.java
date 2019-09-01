fce9a2e346a7922b032d9c55213636cb09d29a8c
Zhibo <zhibo570@gmail.com>
2019-08-31 23:33:17 -0400

    039 transfer infixexpress to suffixexpression

--- Datastructures/src/PolandNotation.java
+++ Datastructures/src/PolandNotation.java
@@ -4,6 +4,18 @@ import java.util.Stack;
 
 public class PolandNotation {
     public static void main(String[] args) {
+        //完成一个中缀表达式转后缀表达式的功能
+        //说明
+        // 1. 1 + ( ( 2 + 3 ) * 4 ) - 5 => 转成1 2 3 + 4 * + 5 -
+        // 2. 因为直接对一个字符串str操作不方便，因此，先将字符串转成中缀表达式对应的list
+        // 3. 即将"1+((2+3)*4)-5" ==> ArrayList[1, +, (, (, 2, +, 3, ), *, 4, ), -, 5]
+
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println(infixExpressionList);
+
+
+        /*
         //先定义逆波兰表达式
         //(3+4)*5-6 => 3 4 + 5 * 6 -
         //测试多位数(30+4)*5-6
@@ -20,6 +32,7 @@ public class PolandNotation {
 
         int res = calculator(rpnList);
         System.out.println("计算的结果：" + res);
+        */
     }
 
     //将一个逆波兰表达式，依次将数据和运算符放入到ArrayList中
@@ -73,5 +86,47 @@ public class PolandNotation {
         }
         //最后留在stack中的数据是运算结果
         return Integer.parseInt(stack.pop());
+    }
+
+    //中缀表达式转后缀表达式的具体步骤：
+    /* 1) 初始化两个栈：运算符栈s1和存储中间结果的栈s2
+     * 2) 从左至右扫描中缀表达式
+     * 3) 遇到操作数时，压入s2
+     * 4) 遇到运算符时，比较其与s1栈顶运算符的优先级：
+     *      1. 如果s1为空，或栈顶运算符为左括号"("，则直接入栈s1
+     *      2. 否则，若优先级比s1栈顶运算符的高，也将其入栈s1
+     *      3. 否则，将s1栈顶的运算符弹出并压入到s2中，再次转到（4-1）与s1中新的栈顶运算符相比较
+     * 5）遇到括号时：
+     *      1. 如果是左括号"("，则直接入栈s1
+     *      2. 如果是右括号")",则依次弹出s1栈顶运算符，并压入s2，直到遇到左括号为止，此时将这一对括号丢弃
+     * 6) 重复步骤2）-5）直到表达式的最右边
+     * 7) 将s1中剩余的运算符依次弹出并压入s2
+     * 8) 依次弹出s2中的元素并输出，结果的逆序即为中缀表达式对应的后缀表达式
+     *
+     */
+// 代码实现:
+
+    //将中缀表达式转换成对应的List
+    public static List<String> toInfixExpressionList(String s) {
+        //先定义一个List，存放中缀表达式对应的内容
+        List<String> ls = new ArrayList<String>();
+        int i = 0; //用于遍历中缀表达式字符串s
+        String str; // 对多位数的拼接
+        char c; //每遍历到一个字符就存入c
+        do {
+            //如果c是一个非数字，我们就需要加入到ls中
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                ls.add("" + c);
+                i++;
+            } else { //如果是一个数，需要考虑多位数问题
+                str = ""; //先将str 置空    '0'【48】-> '9'【57】
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                    str += c; //拼接
+                    i++;
+                }
+                ls.add(str);
+            }
+        } while (i < s.length());
+        return ls;
     }
 }
