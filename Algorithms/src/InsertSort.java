1ebe933200c0ffb27edfe6f82ff8b0e3c6da7fe3
Zhibo <zhibo570@gmail.com>
2019-09-05 21:35:50 -0400

    062 Insert Sorting 2

--- Algorithms/src/InsertSort.java
+++ Algorithms/src/InsertSort.java
@@ -1,9 +1,28 @@
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class InsertSort {
     public static void main(String[] args) {
-        int arr[] = {101, 34, 119, 1};
+//        int arr[] = {101, 34, 119, 1};
+
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 800000);
+        }
+
+        System.out.println("插入排序时间复杂度测试~");
+
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String datestr = simpleDateFormat.format(date);
+        System.out.println("排序前的时间是：" + datestr);
+
         insertSort(arr);
+
+        Date date2 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String datestr2 = simpleDateFormat.format(date2);
+        System.out.println("排序后的时间是：" + datestr2);
     }
 
     //插入排序
@@ -67,16 +86,18 @@ public class InsertSort {
             //1. insertIndex > 0保证在给insertVal找插入位置时，不越界
             //2. insetVal < arr[insetIndex 待插入的数还没有找到合适位置
             //3. 就需要将insertIndex后移
+            // 若要从大到小排列，只需将 < 改成 > 即可
             while (insetIndex >= 0 && insetVal < arr[insetIndex]) {
                 arr[insetIndex + 1] = arr[insetIndex];
                 insetIndex--;
             }
             //当退出循环时，说明插入的位置找到为：  insertIndex+1
-            //
-            arr[insetIndex + 1] = insetVal;
+            //这里判断下是否需要赋值
+            if (insetIndex + 1 != i)
+                arr[insetIndex + 1] = insetVal;
 
-            System.out.println("第 " + i + " 轮插入后：");
-            System.out.println(Arrays.toString(arr));
+//            System.out.println("第 " + i + " 轮插入后：");
+//            System.out.println(Arrays.toString(arr));
         }
 
     }
